08f785ddc88e7ac459c079fc39d7263c6153fe93
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-06-15 01:45:24 -0400

    add cache to problem14

--- 14_Longest_Collatz_sequence.py
+++ 14_Longest_Collatz_sequence.py
@@ -13,16 +13,24 @@
 # NOTE: Once the chain starts the terms are allowed to go above one million.
 
 # Initially, I try to solve it recursively starting from 1, but it causes stack overflow. Use brute force int the end
+
+from collections import deque
+maxLen,maxNum,cache = 0,0,{1:1}
+def cacheChain(chain, rest):
+	global maxLen,maxNum,cache
+	if len(chain)+rest > maxLen:
+		maxLen,maxNum = len(chain)+rest,chain[0]
+	while len(chain):
+		cache[chain[0]] = len(chain)+rest
+		chain.popleft()
+	
 if __name__ == '__main__':
-	maxLen = 0
-	maxNum = 0
-	for n in xrange(1,1000000):
-		count = 1
-		temp = n
+	for n in xrange(2,10**6):
+		chain = deque()
 		while n!=1:
-			count += 1
+			chain.append(n)
 			n = (n/2) if n%2==0 else 3*n+1
-		if count > maxLen:
-			maxLen = count
-			maxNum = temp
+			if n in cache:
+				cacheChain(chain, cache[n])
+				break
 	print maxNum
